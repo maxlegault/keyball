c2a0ac6ed1d0963ec520f6f12eee3594d9872ead
Maxime Legault-Venne <mlegoven@hotmail.com>
2025-05-20 21:48:43 -0400

    Add home row mods in max nav

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/max/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/max/keymap.c
@@ -34,12 +34,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HOME_R LALT_T(KC_R)
 #define HOME_S LCTL_T(KC_S)
 #define HOME_T LSFT_T(KC_T)
+#define HOME_LFT LALT_T(KC_LEFT)
+#define HOME_UP LCTL_T(KC_UP)
+#define HOME_RGT LSFT_T(KC_RGHT)
 
 // Right-hand home row mods
 #define HOME_N RSFT_T(KC_N)
 #define HOME_E RCTL_T(KC_E)
 #define HOME_I LALT_T(KC_I)
 #define HOME_O RGUI_T(KC_O)
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
 
 // Layer mods
 #define TO_TYP TO(LAYER_TYPING)
